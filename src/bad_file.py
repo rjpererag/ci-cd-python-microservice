34e5e998b549d34f09df3d460e03b85e15ece816
Ricardo Perera <ricardoperera@Ricardos-MacBook-Pro.local>
2025-11-26 11:01:43 +0100

    bad file

--- src/bad_file.py
+++ src/bad_file.py
@@ -3,6 +3,3 @@ def thisIsaBADFunction() -> dict:
     too_long_text = "THIS IS MY LONG TEXT -- THIS IS MY LONG TEXT --THIS IS MY LONG TEXT --THIS IS MY LONG TEXT --THIS IS MY LONG TEXT --THIS IS MY LONG TEXT --THIS IS MY LONG TEXT "
 
     return too_long_text.split("--")[0].strip().lower()
-
-
-
